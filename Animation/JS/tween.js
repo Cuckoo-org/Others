6091d866dcf5131e70066d5864a80ca2461e0fd9
hongfei <941116197@qq.com>
2018-11-22 16:45:12 +0800

    动画库支持完成回调

--- Animation/JS/tween.js
+++ Animation/JS/tween.js
@@ -5,7 +5,7 @@
       }
   };
   //->move:实现多方向的运动动画 curEle->当前要操作运动的元素 target->当前动画的目标位置,存储的是每一个方向的目标位置{left:xxx,top:xxx ....},duration当前动画的总时间
-  function move(curEle,target,duration){
+  function move(curEle,target,duration,callBack){
     //->在每一次执行方法之前，首先把当前元素之前正在运行的动画结束掉
     window.clearInterval(curEle.contonTimer);
 
@@ -25,6 +25,8 @@
          if(time>=duration){
              utils.css(curEle,target);
              window.clearInterval(curEle.contonTimer);
+            //->当动画结束时候，如果用户把回调函数传递给我了，我就把用户传递的那个函数执行,不仅执行还让回调函数中的this变为当前要操作的元素
+           callBack && callBack.call(curEle);
              return;
          }
          //->没到达目标：分别获取每一个方向的当前位置，给当前元素设置样式即可
