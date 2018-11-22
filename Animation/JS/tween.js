579ad6e1482c251dc78bfd381fdad8243a21dc85
hongfei <941116197@qq.com>
2018-11-22 16:08:58 +0800

    简单的动画库

---
+++ Animation/JS/tween.js
@@ -0,0 +1,40 @@
+~function(){
+  var contonEffect = {
+      Linear:function(t,b,c,d){
+          return c*t/d+b;
+      }
+  };
+  //->move:实现多方向的运动动画 curEle->当前要操作运动的元素 target->当前动画的目标位置,存储的是每一个方向的目标位置{left:xxx,top:xxx ....},duration当前动画的总时间
+  function move(curEle,target,duration){
+    //->在每一次执行方法之前，首先把当前元素之前正在运行的动画结束掉
+    window.clearInterval(curEle.contonTimer);
+
+   //->根据target获取每一个方向的起始值begin和总距离change
+    var begin = {},change = {};
+    for(var key in target){
+        if(target.hasOwnProperty(key)){
+            begin[key] = utils.css(curEle,key);
+            change[key]=target[key]-begin[key];
+        }
+    }
+    //->实现多方向的运动动画
+     var time = 0;
+     curEle.contonTimer=window.setInterval(function(){
+         time+=10;
+         //->到达目标：结束动画，让当前元素的样式等于目标样式值
+         if(time>=duration){
+             utils.css(curEle,target);
+             window.clearInterval(curEle.contonTimer);
+             return;
+         }
+         //->没到达目标：分别获取每一个方向的当前位置，给当前元素设置样式即可
+         for(var key in target){
+            if(target.hasOwnProperty(key)){
+                var curPos = contonEffect.Linear(time,begin[key],change[key],duration);
+                utils.css(curEle,key,curPos)
+            }
+         }
+        },10)
+ }
+ window.contonAnimate = move;
+}();
